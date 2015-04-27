ac26de31a0f999213e15900dd89e392ca801dc12
Erik Ostrom <erik@echographia.com>
2015-04-27 18:54:32 -0500

    Switch to ES6 via Babel.

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.get('/', (req, res) => res.send('Hello World!'));
 
-var server = app.listen(3000, function () {
+let server = app.listen(3000, () => {
+  let host = server.address().address;
+  let port = server.address().port;
 
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+  console.log(`Example app listening at http://${host}:${port}`);
 });
