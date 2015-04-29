6ead1608650a89a35999c2ba9f63d2f89047865a
Erik Ostrom <erik@echographia.com>
2015-04-29 15:51:48 -0500

    Split `index` into `app` and `server`.
    `app.js` defines the Express application; `server.js` starts a server
    that runs it.
    
    Soon we'll have acceptance tests that run the application without
    explicitly starting a server.

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
-let express = require('express');
-let app = express();
-
-app.get('/', (req, res) => res.send('Hello World!'));
+let app = require('./app');
 
 let server = app.listen(3000, () => {
   let host = server.address().address;
